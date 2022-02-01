9d77888d8489cb6c10489341133258b88e9db5a4
Giuseppe Gallo Cassarino <giuseppegallocassarino@MBP-di-Giuseppe.home-life.hub>
2022-02-01 23:23:47 +0100

    esercizio

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.scss';
+import Notes from './components/Notes';
+
+function App() {
+  return (
+    <div className="App">
+      <Notes />
+      ......
+    </div>
+  );
+}
+
+export default App;
